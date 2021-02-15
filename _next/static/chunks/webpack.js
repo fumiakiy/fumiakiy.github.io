6fbca6da9ef799909232add042220304fa304c3a
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2021-02-15 09:18:24 -0500

    Updated.

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8b13f1d8466a57975385";
+/******/ 	var hotCurrentHash = "c6593cc4eb500c1dbe87";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
